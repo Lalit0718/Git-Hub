42f377606224f962bd0f784f34c357f9d1e32fd2
Lalit Patil <lalitrpatil2002@gmail.com>
2026-08-29 16:25:30 +0530

    footer added

--- first.js
+++ first.js
@@ -2,4 +2,7 @@ const button = "Added a button";
 console.log(button);
 
 
-const login = "login page added"
+const login = "login page added";
+
+const footer = "Footer added in our website";
+console.log(footer);
